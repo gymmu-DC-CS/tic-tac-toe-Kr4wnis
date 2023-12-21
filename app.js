ad2537b9d07c701916e62b2c43dc8bf171b302f7
Jimmy <e220859@edu.sbl.ch>
2023-12-21 11:25:23 +0100

    bis zu pause 12 min beim video

---
+++ app.js
@@ -0,0 +1,14 @@
+const boxes = Array.from(document.getElementsByClassName('box'));
+console.log(boxes);
+
+const drawBoard = () =>{
+    boxes.forEach((box, index) => {
+        let styleString = '';
+        if (index < 3) {
+            styleString += `border-bottom: 3px solid var(--purple)`;
+        }
+        if(index %$ 3 === 0){
+            styleString += `border-bottom: 3px solid var(--purple)`;
+        }
+    })
+}
